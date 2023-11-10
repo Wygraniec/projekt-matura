1fb485305466836340f82c92d97d044e7abc1507
HubertM6 <119881354+HubertM6@users.noreply.github.com>
2023-11-10 18:05:28 +0100

    Created the main java project

---
+++ backend/matura/src/main/java/pl/lodz/p/liceum/matura/MaturaApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.liceum.matura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaturaApplication.class, args);
+    }
+
+}
---
+++ backend/matura/src/test/java/pl/lodz/p/liceum/matura/MaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.liceum.matura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
