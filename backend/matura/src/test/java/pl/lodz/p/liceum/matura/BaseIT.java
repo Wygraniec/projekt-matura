df7af7f16c4b32f57757939d27acac904584208b
HubertM6 <119881354+HubertM6@users.noreply.github.com>
2023-11-24 17:21:10 +0100

    Added tests

---
+++ backend/matura/src/test/java/pl/lodz/p/liceum/matura/BaseIT.java
@@ -0,0 +1,169 @@
+package pl.lodz.p.liceum.matura;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pl.lodz.p.liceum.matura.domain.user.User;
+import pl.lodz.p.liceum.matura.domain.user.UserRole;
+import pl.lodz.p.liceum.matura.domain.user.UserService;
+import pl.lodz.p.liceum.matura.external.storage.user.JpaUserRepository;
+import pl.lodz.p.liceum.matura.security.JWTUtil;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ActiveProfiles("it")
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = MaturaApplication.class
+)
+@ExtendWith(SpringExtension.class)
+public class BaseIT {
+
+    @Autowired
+    protected Environment environment;
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    @Autowired
+    protected UserService userService;
+
+    protected BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    protected AuthenticationManager authenticationManager;
+
+    @Autowired
+    protected JWTUtil jwtUtil;
+
+    @Autowired
+    private ServerPortService serverPortService;
+
+    @Autowired
+    private JpaUserRepository jpaUserRepository;
+    @BeforeEach
+    void init() {
+        jpaUserRepository.deleteAll();
+        addTestUsers();
+    }
+
+    private User adminUser = new User(
+            null,
+            "admin@gmail.com",
+            "John",
+            "password",
+            UserRole.ADMIN,
+            ZonedDateTime.of(
+                    2023,
+                    6,
+                    17,
+                    12,
+                    30,
+                    20,
+                    0,
+                    ZoneId.of("UTC"))
+    );
+
+    private User instructorUser = new User(
+            null,
+            "technologist@gmail.com",
+            "John",
+            "password",
+            UserRole.INSTRUCTOR,
+            ZonedDateTime.of(
+                    2023,
+                    6,
+                    17,
+                    12,
+                    30,
+                    20,
+                    0,
+                    ZoneId.of("UTC"))
+    );
+
+    private User studentUser = new User(
+            null,
+            "md@gmail.com",
+            "John",
+            "password",
+            UserRole.STUDENT,
+            ZonedDateTime.of(
+                    2023,
+                    6,
+                    17,
+                    12,
+                    30,
+                    20,
+                    0,
+                    ZoneId.of("UTC"))
+    );
+
+    protected String localUrl(String endpoint) {
+        int port = serverPortService.getPort();
+        return "http://localhost:" + port + endpoint;
+    }
+
+    protected void addTestUsers() {
+        userService.save(adminUser);
+        userService.save(instructorUser);
+        userService.save(studentUser);
+    }
+
+    protected String getAccessTokenForUser(User user) {
+        String token = jwtUtil.issueToken(user.getEmail(), "ROLE_" + user.getRole());
+        return "Bearer " + token;
+    }
+
+    protected String getAccessTokenForAdmin() {
+        String token = jwtUtil.issueToken(adminUser.getEmail(), "ROLE_" + adminUser.getRole());
+        return "Bearer " + token;
+    }
+
+    protected String getAccessTokenForTechnologist() {
+        String token = jwtUtil.issueToken(instructorUser.getEmail(), "ROLE_" + instructorUser.getRole());
+        return "Bearer " + token;
+    }
+
+    protected String getAccessTokenForMD() {
+        String token = jwtUtil.issueToken(studentUser.getEmail(), "ROLE_" + studentUser.getRole());
+        return "Bearer " + token;
+    }
+
+    protected <T, U> ResponseEntity<U> callHttpMethod(
+            HttpMethod httpMethod,
+            String url,
+            String accessToken,
+            T body,
+            Class<U> mapToObject
+    ) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+        headers.add(HttpHeaders.AUTHORIZATION, accessToken);
+        headers.add(HttpHeaders.ACCEPT, "application/json");
+        HttpEntity<T> requestEntity;
+        if (body == null) {
+            requestEntity = new HttpEntity<>(headers);
+        } else {
+            requestEntity = new HttpEntity<>(body, headers);
+        }
+        return restTemplate.exchange(
+                localUrl(url),
+                httpMethod,
+                requestEntity,
+                mapToObject
+        );
+    }
+
+}
---
+++ backend/matura/src/test/java/pl/lodz/p/liceum/matura/api/user/UserControllerIT.java
@@ -0,0 +1,313 @@
+package pl.lodz.p.liceum.matura.api.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import pl.lodz.p.liceum.matura.BaseIT;
+import pl.lodz.p.liceum.matura.TestUserFactory;
+import pl.lodz.p.liceum.matura.api.response.ErrorResponse;
+import pl.lodz.p.liceum.matura.domain.user.User;
+import pl.lodz.p.liceum.matura.domain.user.UserService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerIT extends BaseIT {
+
+    @Autowired
+    UserService service;
+
+    @Test
+    void admin_should_get_information_about_any_user() {
+        //given
+        User user = TestUserFactory.createInstructor();
+        service.save(user);
+        String token = getAccessTokenForAdmin();
+
+        //when
+        var response = callHttpMethod(HttpMethod.GET,
+                "/api/v1/users/" + service.findByEmail(user.getEmail()).getId(),
+                token,
+                null,
+                UserDto.class);
+
+        //then
+        UserDto body = response.getBody();
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertNotNull(body);
+        assertEquals(user.getEmail(), body.email());
+        assertEquals(user.getNickname(), body.nickname());
+        assertEquals("######", body.password());
+        assertEquals(user.getRole().toString(), body.role().toString());
+    }
+
+    @Test
+    void admin_should_get_response_code_404_when_user_not_exits_in_db() {
+        //given
+        String token = getAccessTokenForAdmin();
+
+        //when
+        var response = callHttpMethod(HttpMethod.GET,
+                "/api/v1/users/10",
+                token,
+                null,
+                ErrorResponse.class);
+
+        //then
+        assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void user_should_not_get_information_about_other_user() {
+        //given
+        User user1 = TestUserFactory.createInstructor();
+        User user2 = TestUserFactory.createStudent();
+        service.save(user1);
+        service.save(user2);
+        String accessToken = getAccessTokenForUser(user1);
+
+        //when
+        var response = callHttpMethod(HttpMethod.GET,
+                "/api/v1/users/" + service.findByEmail(user2.getEmail()).getId(),
+                accessToken,
+                null,
+                ErrorResponse.class);
+
+        //then
+        assertEquals(response.getStatusCode(), HttpStatus.FORBIDDEN);
+    }
+
+//    @Test
+//    void admin_should_get_response_code_conflict_when_user_is_in_db() {
+//        //given
+//        User user = TestUserFactory.createTechnologist();
+//        service.save(user);
+//        String adminToken = getAccessTokenForAdmin();
+//
+//        //when
+//        var response = callHttpMethod(HttpMethod.POST,
+//                "/api/v1/users",
+//                adminToken,
+//                user,
+//                ErrorResponse.class);
+//
+//        //then
+//        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+//    }
+
+
+    @Test
+    void admin_should_be_able_to_save_new_user() {
+        //given
+        User user = TestUserFactory.createInstructor();
+        String adminAccessToken = getAccessTokenForAdmin();
+
+        //when
+        var response = callHttpMethod(HttpMethod.POST,
+                "/api/v1/users",
+                adminAccessToken,
+                user,
+                UserDto.class);
+
+        //then
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        //and
+        UserDto body = response.getBody();
+        assertNotNull(body);
+        assertEquals(body.email(), user.getEmail());
+        assertEquals(body.nickname(), user.getNickname());
+        assertEquals(body.password(), "######");
+        assertEquals(body.role().toString(), user.getRole().toString());
+    }
+
+    @Test
+    void user_should_get_information_about_himself() {
+        //given
+        User user = TestUserFactory.createInstructor();
+        service.save(user);
+        String accessToken = getAccessTokenForUser(user);
+
+        //when
+        var response = callHttpMethod(HttpMethod.GET,
+                "/api/v1/users/me",
+                accessToken,
+                null,
+                UserDto.class);
+
+        //then
+        UserDto body = response.getBody();
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        //and
+        assertNotNull(body);
+        assertEquals(body.email(), user.getEmail());
+        assertEquals(body.nickname(), user.getNickname());
+        assertEquals(body.password(), "######");
+        assertEquals(body.role().toString(), user.getRole().toString());
+    }
+
+    @Test
+    void admin_should_be_able_to_update_user() {
+        //given
+        User user = TestUserFactory.createInstructor();
+        userService.save(user);
+        User toUpdate = new User(
+                user.getId(),
+                "email@email.com",
+                "newPerson",
+                "newpassword",
+                user.getRole(),
+                user.getCreatedAt()
+        );
+        String adminAccessToken = getAccessTokenForAdmin();
+
+        //when
+        var response = callHttpMethod(HttpMethod.PUT,
+                "/api/v1/users",
+                adminAccessToken,
+                toUpdate,
+                UserDto.class);
+
+        //then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void admin_should_be_get_response_code_200_when_update_user_not_exits() {
+        //given
+        String token = getAccessTokenForAdmin();
+        User fakeUser = TestUserFactory.createInstructor();
+
+        //when
+        var response = callHttpMethod(HttpMethod.PUT,
+                "/api/v1/users",
+                token,
+                fakeUser,
+                ErrorResponse.class);
+
+        //then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void user_should_be_not_able_to_update_user() {
+        //given
+        User user = TestUserFactory.createInstructor();
+        userService.save(user);
+        User userToUpdate = new User(
+                user.getId(),
+                "otherUser@email.com",
+                "Person",
+                "password",
+                user.getRole(),
+                user.getCreatedAt()
+        );
+        String token = getAccessTokenForUser(user);
+
+        //when
+        var response = callHttpMethod(HttpMethod.PUT,
+                "/api/v1/users",
+                token,
+                userToUpdate,
+                ErrorResponse.class);
+
+        //then
+        assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    }
+
+    @Test
+    void admin_should_be_able_to_delete_user() {
+        //given
+        User user = TestUserFactory.createInstructor();
+        String adminAccessToken = getAccessTokenForAdmin();
+        userService.save(user);
+
+        //when
+        var response = callHttpMethod(
+                HttpMethod.DELETE,
+                "/api/v1/users/" + service.findByEmail(user.getEmail()).getId(),
+                adminAccessToken,
+                null,
+                UserDto.class);
+
+        //then
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+    }
+
+    @Test
+    void admin_should_get_response_code_204_when_user_not_exits() {
+        //given
+        User user = TestUserFactory.createInstructor();
+        String token = getAccessTokenForAdmin();
+
+        //when
+        var response = callHttpMethod(
+                HttpMethod.DELETE,
+                "/api/v1/users/" + user.getId(),
+                token,
+                null,
+                ErrorResponse.class);
+
+        //then
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+    }
+    //
+    @Test
+    void device_owner_should_not_be_able_to_delete_user() {
+        //given
+        User firstUser = TestUserFactory.createInstructor();
+        User secondUser = TestUserFactory.createStudent();
+        userService.save(firstUser);
+        userService.save(secondUser);
+        String token = getAccessTokenForUser(firstUser);
+
+        //when
+        var response = callHttpMethod(
+                HttpMethod.DELETE,
+                "/api/v1/users/" + secondUser.getId(),
+                token,
+                null,
+                ErrorResponse.class);
+
+        //then
+        assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    }
+
+    @Test
+    void admin_should_get_pageable_list_of_users() {
+        //give
+        User user1 = TestUserFactory.createInstructor();
+        User user2 = TestUserFactory.createInstructor();
+        User user3 = TestUserFactory.createInstructor();
+        User user4 = TestUserFactory.createInstructor();
+        String adminAccessToken = getAccessTokenForAdmin();
+        userService.save(user1);
+        userService.save(user2);
+        userService.save(user3);
+        userService.save(user4);
+
+        //when
+        var response = callHttpMethod(
+                HttpMethod.GET,
+                "/api/v1/users",
+                adminAccessToken,
+                null,
+                PageUserDto.class
+        );
+
+        //then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        PageUserDto body = response.getBody();
+        //and
+        assertNotNull(body);
+        assertEquals(6, body.users().size());
+        assertEquals(7, body.totalElements());
+        assertEquals(2, body.totalPages());
+        assertEquals(1, body.currentPage());
+        //and users passwords should be hashed
+        assertTrue(
+                body.users().stream()
+                        .allMatch(userDto -> userDto.password().equals("######"))
+        );
+    }
+
+}
