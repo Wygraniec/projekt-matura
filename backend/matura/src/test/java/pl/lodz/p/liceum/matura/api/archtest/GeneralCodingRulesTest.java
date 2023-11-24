5b2b4f73e72d82da6abf4952b7fdf6d475366053
rafal-nowak <contact@rafal-nowak.com>
2023-11-24 22:20:33 +0100

    beans are not injected by field

--- backend/matura/src/test/java/pl/lodz/p/liceum/matura/api/archtest/GeneralCodingRulesTest.java
+++ backend/matura/src/test/java/pl/lodz/p/liceum/matura/api/archtest/GeneralCodingRulesTest.java
@@ -17,9 +17,9 @@ class GeneralCodingRulesTest {
     static final ArchRule no_classes_should_throw_generic_exceptions =
             GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
 
-    /*@ArchTest
+    @ArchTest
     static final ArchRule beans_should_not_be_injected_by_field =
-            GeneralCodingRules.NO_CLASSES_SHOULD_USE_FIELD_INJECTION;*/
+            GeneralCodingRules.NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
 
     @ArchTest
     static final ArchRule deprecated_classes_should_not_be_used = noClasses().that()
