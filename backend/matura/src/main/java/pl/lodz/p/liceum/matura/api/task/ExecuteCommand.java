85b26f8811c8f96c7eaa4b03651833966d9acaa1
HubertM6 <119881354+HubertM6@users.noreply.github.com>
2023-12-18 20:38:41 +0100

    Work in progress subtasks testing system

--- backend/matura/src/main/java/pl/lodz/p/liceum/matura/api/task/ExecuteCommand.java
+++ backend/matura/src/main/java/pl/lodz/p/liceum/matura/api/task/ExecuteCommand.java
@@ -1,7 +1,5 @@
 package pl.lodz.p.liceum.matura.api.task;
 
-import pl.lodz.p.liceum.matura.domain.task.TestType;
-
 public record ExecuteCommand(
         String workspaceUrl,
         String name,
