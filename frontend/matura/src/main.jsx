d8ff153f9c00f30b09983c3f2aefc12a04f48de4
Wygraniec <wojciech.legocki@gmail.com>
2024-03-14 17:34:12 +0100

    Created react project and installed react router

---
+++ frontend/matura/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './Home.jsx'
+import './index.css'
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
