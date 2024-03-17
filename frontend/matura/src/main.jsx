a94fe99cdd501749f6de69e224844f5fb43585c6
Wygraniec <wojciech.legocki@gmail.com>
2024-03-17 21:30:55 +0100

    Designed the login form

--- frontend/matura/src/main.jsx
+++ frontend/matura/src/main.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Home.jsx'
-import './index.css'
 
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Login.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Home/>,
     },
+    {
+        path: "/login",
+        element: <Login/>,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ChakraProvider>
+            <RouterProvider router={router}/>
+        </ChakraProvider>
+    </React.StrictMode>,
 )
