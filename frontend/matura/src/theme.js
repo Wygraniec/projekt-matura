33c6cdcf58162e7ff6193f580ddcd859f757851a
HubertM6 <119881354+HubertM6@users.noreply.github.com>
2024-03-23 21:28:06 +0100

    Added default system color mode

---
+++ frontend/matura/src/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+    config: {
+        initialColorMode: 'system',
+        useSystemColorMode: 'true'
+    },
+})
+
+export default theme
