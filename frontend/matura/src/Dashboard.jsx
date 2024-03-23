1cc70c8797780c070e87d49cb8a7d18186f50a1e
Wygraniec <wojciech.legocki@gmail.com>
2024-03-23 22:38:54 +0100

    Created a navbar and added an option to change color mode

--- frontend/matura/src/Dashboard.jsx
+++ frontend/matura/src/Dashboard.jsx
@@ -1,18 +1,10 @@
-import {logout, User} from "./services/userService.js";
 import {withAuthentication} from "./routeAuthentication.jsx";
+import {Navbar} from "./components/Navbar.jsx";
 
 const Dashboard = () => {
-    const user = User.fromLocalStorage();
-
     return (
         <>
-            <h2>Zalogowano</h2>
-            <p>{user.id}</p>
-            <p>{user.username}</p>
-            <p>{user.email}</p>
-            <p>{user.role}</p>
-
-            <button onClick={logout}>Wyloguj</button>
+            <Navbar/>
         </>
     )
 }
--- frontend/matura/src/theme.js
+++ frontend/matura/src/theme.js
@@ -3,7 +3,7 @@ import { extendTheme } from '@chakra-ui/react'
 const theme = extendTheme({
     config: {
         initialColorMode: 'system',
-        useSystemColorMode: 'true'
+        // useSystemColorMode: 'true'
     },
 })
 
