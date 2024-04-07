6d649d812e98565a8f18d7150d3d2b0437611ea0
HubertM6 <119881354+HubertM6@users.noreply.github.com>
2024-04-07 18:30:31 +0200

    Added DefaultTemplates.java

---
+++ backend/matura/src/main/java/pl/lodz/p/liceum/matura/DefaultTemplates.java
@@ -0,0 +1,81 @@
+package pl.lodz.p.liceum.matura;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import pl.lodz.p.liceum.matura.domain.template.TaskLanguage;
+import pl.lodz.p.liceum.matura.domain.template.Template;
+import pl.lodz.p.liceum.matura.domain.template.TemplateService;
+import pl.lodz.p.liceum.matura.domain.user.User;
+import pl.lodz.p.liceum.matura.domain.user.UserRole;
+
+import java.time.ZonedDateTime;
+
+@Component
+@Log
+public class DefaultTemplates implements CommandLineRunner {
+
+    private final TemplateService templateService;
+
+    public DefaultTemplates(TemplateService templateService) {
+        this.templateService = templateService;
+    }
+
+    private final Template matura05_2023Python = new Template(
+            null,
+            "https://github.com/HubertM6/MaturaBinaryNumbersPython",
+            TaskLanguage.PYTHON,
+            "Matura 05.2023",
+            0,
+            ZonedDateTime.now()
+    );
+    private final Template matura05_2023CSharp = new Template(
+            null,
+            "https://github.com/HubertM6/MaturaBinaryNumbersCSharp",
+            TaskLanguage.C_SHARP,
+            "Matura 05.2023",
+            0,
+            ZonedDateTime.now()
+    );
+    private final Template matura05_2023Java = new Template(
+            null,
+            "https://github.com/HubertM6/MaturaBinaryNumbersJava",
+            TaskLanguage.JAVA,
+            "Matura 05.2023",
+            0,
+            ZonedDateTime.now()
+    );
+    private final Template matura05_2022Python = new Template(
+            null,
+            "https://github.com",
+            TaskLanguage.PYTHON,
+            "Matura 05.2023",
+            0,
+            ZonedDateTime.now()
+    );
+    private final Template OI31Etap1BudowaLotniskaPython = new Template(
+            null,
+            "https://github.com",
+            TaskLanguage.PYTHON,
+            "31 OI Etap 1 Budowa lotniska",
+            0,
+            ZonedDateTime.now()
+    );
+
+    @Override
+    public void run(String... args) {
+        try {
+            addTemplate(matura05_2023Python);
+            addTemplate(matura05_2023CSharp);
+            addTemplate(matura05_2023Java);
+            addTemplate(matura05_2022Python);
+            addTemplate(OI31Etap1BudowaLotniskaPython);
+        } catch (Exception ex) {
+            log.warning(ex.getMessage());
+        }
+    }
+
+    private void addTemplate(Template template) {
+        templateService.save(template);
+    }
+}
