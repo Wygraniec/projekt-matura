76a3bc90029ea31ef795c88acde646489b7ad427
Wygraniec <wojciech.legocki@gmail.com>
2024-06-28 14:57:19 +0200

    Moved RenderMarkdown.jsx do its own component

--- frontend/matura/src/TaskList.jsx
+++ frontend/matura/src/TaskList.jsx
@@ -8,17 +8,16 @@ import {
     Button,
     Card,
     CardBody,
-    CardHeader, Divider,
-    Flex,
+    CardHeader, Flex,
     Image, Input, Select,
     Spinner,
     Text, useToast
 } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import {motion} from 'framer-motion'
-import ReactMarkdown from "react-markdown";
 import {Formik, Form} from "formik";
 import {PaginationLinks} from "./components/PaginationLinks.jsx";
+import {RenderMarkdown} from "./components/RenderMarkdown.jsx";
 
 const LanguageIcon = ({language, ...props}) => {
     switch (language) {
@@ -85,49 +84,7 @@ const TemplateCard = ({template, ...props}) => {
                     }}
                 >
                     <CardBody display='flex' justifyContent='center'>
-                        <Box
-                            borderRadius="md"
-                            maxWidth="95%"
-                            width="full"
-                            marginX="25px"
-                            borderColor='gray'
-                            borderWidth="1px"
-                            borderStyle="solid"
-                            paddingX="2.5em"
-                            paddingY='1.5em'
-                        >
-                            <ReactMarkdown components={{
-                                paragraph: (props) => <Text as='p' {...props}/>,
-
-                                h1: (props) => <Text as='h1' fontSize='3xl' fontWeight='bold' {...props} />,
-                                h2: (props) => <Text as='h2' fontSize='2xl' fontWeight='bold' {...props} />,
-                                h3: (props) => <Text as='h3' fontSize='xl' fontWeight='bold' {...props} />,
-                                h4: (props) => <Text as='h4' fontSize='lg' fontWeight='bold' {...props} />,
-                                h5: (props) => <Text as='h5' fontSize='md' fontWeight='bold' {...props} />,
-                                h6: (props) => <Text as='h6' fontSize='sm' fontWeight='bold' {...props} />,
-
-                                list: (props) => <Box as='ul' {...props} />,
-                                listItem: (props) => <Box as='li' {...props} />,
-                                orderedList: (props) => <Box as='ol' {...props} />,
-
-                                code: (props) => <Text as='code' {...props} />,
-                                inlineCode: (props) => <Text as='code' fontSize='sm' {...props} />,
-                                blockquote: (props) => <Box as='blockquote' borderLeft='4px solid gray.300'
-                                                            p={4} {...props} />,
-
-                                strong: (props) => <Text as='strong' fontWeight='bold' {...props} />,
-                                em: (props) => <Text as='em' fontStyle='italic' {...props} />,
-
-                                hr: (props) => <Divider {...props}/>,
-
-                                link: (props) => (
-                                    <Text as='a' color='blue.500'
-                                          _hover={{textDecoration: 'underline'}} {...props} />
-                                )
-                            }}>
-                                {template.statement}
-                            </ReactMarkdown>
-                        </Box>
+                        <RenderMarkdown document={template.statement}/>
                     </CardBody>
                 </motion.div>
             )}
