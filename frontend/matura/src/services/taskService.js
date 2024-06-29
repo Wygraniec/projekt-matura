007a4cbef2792bba47ea91c8dbb078fad2e1da3b
Wygraniec <wojciech.legocki@gmail.com>
2024-06-29 19:39:11 +0200

    Made user able to solve task based on a template from the /tasklist endpoint

--- frontend/matura/src/services/taskService.js
+++ frontend/matura/src/services/taskService.js
@@ -32,9 +32,29 @@ export class Task {
         )
     }
 
+    // TODO when backend is ready, check for existing tasks on the backend
+    // TODO when backend is ready, get numberOfSubtasks from the backend
+    /**
+     * Returns id of a task, if there exists one (one being assigned to a specific user with a given template)
+     * @param templateId id of the template, which user wants to solve
+     * */
+    static async createOrGet(templateId) {
+        const response = await axios.post(
+            `${API}/tasks`,
+            {
+                templateId: templateId,
+                numberOfSubtasks: 3,
+                userId: User.fromLocalStorage().id
+            },
+            User.fromLocalStorage().getAuthHeader()
+        ).catch(e => { console.log(e) })
+
+        return response.data['id']
+    }
+
     async getFile() {
         let endpoint = `${API}/tasks/${this.id}/file`
-        const response = await axios.get(endpoint, User.fromLocalStorage().getAuthHeader({ responseType: 'text' }));
+        const response = await axios.get(endpoint, User.fromLocalStorage().getAuthHeader({responseType: 'text'}));
 
         return response.data;
     }
@@ -42,7 +62,7 @@ export class Task {
     async saveFile(fileContents) {
         let endpoint = `${API}/tasks/${this.id}/file`
 
-        const blob = new Blob([fileContents], { type: 'text/plain' })
+        const blob = new Blob([fileContents], {type: 'text/plain'})
         const formData = new FormData();
         formData.append('file', blob, 'task.py')
 
@@ -56,7 +76,7 @@ export class Task {
                     }
                 }))
             return true
-        } catch(e) {
+        } catch (e) {
             return false
         }
     }
