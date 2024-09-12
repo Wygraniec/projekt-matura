663fffb89d81eeccc8495c7a9f8a0d176618660e
HubertM6 <119881354+HubertM6@users.noreply.github.com>
2024-09-12 17:49:04 +0200

    Created accounts for students, added another template

--- backend/matura/src/main/java/pl/lodz/p/liceum/matura/DefaultTemplates.java
+++ backend/matura/src/main/java/pl/lodz/p/liceum/matura/DefaultTemplates.java
@@ -90,6 +90,62 @@ public class DefaultTemplates implements CommandLineRunner {
             0,
             ZonedDateTime.now()
     );
+    private final Template matura06_2023Python = new Template(
+            null,
+            "https://github.com/HubertM6/Matura2023JuneTask3",
+            TaskLanguage.PYTHON,
+            "Matura 06.2023",
+            """
+                    # Anagram binarny
+                                        
+                    Każdy wiersz pliku tekstowego, którego ścieżka przekazana jest jako argument funkcji, zawiera liczbę binarną, składającą się z maksymalnie 14 cyfr. Każda liczba zaczyna się jedynką i żadna z nich się nie powtarza.
+                                        
+                    ## Podzadanie 1
+                    Liczbę binarną nazywamy *zrównoważoną*, gdy zawiera tyle samo zer i jedynek, natomiast *prawie zrównoważoną*, gdy liczba jedynek różni się od liczby zer o 1.
+                                        
+                    **Przykład:** \s
+                    Liczba 101010 jest liczbą *zrównoważoną* \s
+                    Liczba 1011010 jest liczbą *prawie zrównoważoną*
+                                        
+                    Podaj, ile liczb zrównoważonych, a ile prawie zrównoważonych, zapisanych jest w podanym pliku tekstowym (patrz: wskazówka dotyczącą zwracania wielu wartości z funkcji w pythonie).
+                                        
+                    ## Podzadanie 2
+                    *Anagramy cyfrowe* to liczby utworzone z tego samego zestawu cyfr ustawionych w różnych kolejnościach. Przy tym pierwsza cyfra liczby nie może być równa zero.
+                                        
+                    **Przykład:**   \s
+                    Z liczby 209 zapisanej dziesiętnie można utworzyć 4 anagramy: 209, 902, 290, 920. \s
+                    Z liczby binarnej 11100 można utworzyć 6 różnych anagramów: 10011, 10101, 10110, 11001, 11010, 11100.
+                                        
+                    Znajdź wszystkie takie liczby binarne 8-cyfrowe w pliku, z których można utworzyć największą liczbę anagramów. Zwróć je w postaci listy, zawierającej je w takiej kolejności, w jakiej występują one w podanym pliku tekstowym.
+                                        
+                    ## Podzadanie 3
+                    Podaj największą wartość bezwzględną różnicy między sąsiednimi liczbami (to jest liczbami zapisanymi w sąsiednich wierszach np. 1 i 2 wierszu, 2 i 3 wierszu itd.) w podanym pliku tekstowym. Podaj tę wartość w zapisie binarnym.
+                                        
+                    ## Podzadanie 4
+                    Zamień wszystkie liczby binarne z podanego pliku tekstowego na ich odpowiedniki w systemie dziesiętnym. Następnie spośród otrzymanych liczb dziesiętnych:
+                    1. podaj, ile jest takich, w zapisie których nie występuje cyfra zero
+                    2. podaj liczbę, która ma największą sumę różnych cyfr (jeśli liczb, które mają tę samą, największą sumę różnych cyfr, jest więcej niż jedna – podaj tę, która występuje jako pierwsza w pliku z danymi)
+                                        
+                    **Przykład:**   \s
+                    Dla liczby 20462 suma jej różnych cyfr to 12 (2+0+4+6)  \s
+                    Dla liczby 344 suma różnych cyfr to 7.
+                                        
+                    Patrz: wskazówka dotyczącą zwracania wielu wartości z funkcji w pythonie
+                                        
+                    # Zwracanie wielu wartości z funkcji w pythonie
+                    Żeby zwrócić kilka wartości z funkcji w pythonie można zastosować krotkę (tuple), na przykład:
+                    ```python
+                    def func():
+                        var1 = 5
+                        var2 = 10
+                                        
+                        return var1, var2
+                    ```
+                    """,
+            4,
+            0,
+            ZonedDateTime.now()
+    );
     private final Template matura05_2023CSharp = new Template(
             null,
             "https://github.com/HubertM6/MaturaBinaryNumbersCSharp",
@@ -135,10 +191,11 @@ public class DefaultTemplates implements CommandLineRunner {
     public void run(String... args) {
         try {
             addTemplate(matura05_2023Python);
-            addTemplate(matura05_2023CSharp);
-            addTemplate(matura05_2023Java);
-            addTemplate(matura05_2022Python);
-            addTemplate(OI31Etap1BudowaLotniskaPython);
+//            addTemplate(matura05_2023CSharp);
+//            addTemplate(matura05_2023Java);
+//            addTemplate(matura05_2022Python);
+//            addTemplate(OI31Etap1BudowaLotniskaPython);
+            addTemplate(matura06_2023Python);
         } catch (Exception ex) {
             log.warning(ex.getMessage());
         }
--- backend/matura/src/main/java/pl/lodz/p/liceum/matura/DefaultUsers.java
+++ backend/matura/src/main/java/pl/lodz/p/liceum/matura/DefaultUsers.java
@@ -48,6 +48,123 @@ public class DefaultUsers implements CommandLineRunner {
             0,
             ZonedDateTime.now()
     );
+    private final User test1 = new User(
+            null,
+            "hubert@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test2 = new User(
+            null,
+            "wojciechl@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test3 = new User(
+            null,
+            "krzysztof@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test4 = new User(
+            null,
+            "jakub@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test5 = new User(
+            null,
+            "oskar@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test6 = new User(
+            null,
+            "ola@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test7 = new User(
+            null,
+            "tomasz@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test8 = new User(
+            null,
+            "pawel@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test9 = new User(
+            null,
+            "dawid@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test10 = new User(
+            null,
+            "laura@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test11 = new User(
+            null,
+            "miachal@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test12 = new User(
+            null,
+            "tymek@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
+    private final User test13 = new User(
+            null,
+            "wojciechg@gmail.com",
+            "Test",
+            "password",
+            UserRole.STUDENT,
+            0,
+            ZonedDateTime.now()
+    );
 
     @Override
     public void run(String... args) {
@@ -55,6 +172,19 @@ public class DefaultUsers implements CommandLineRunner {
             addUser(adminUser);
             addUser(studentUser);
             addUser(instructorUser);
+            addUser(test1);
+            addUser(test2);
+            addUser(test3);
+            addUser(test4);
+            addUser(test5);
+            addUser(test6);
+            addUser(test7);
+            addUser(test8);
+            addUser(test9);
+            addUser(test10);
+            addUser(test11);
+            addUser(test12);
+            addUser(test13);
         } catch (Exception ex) {
             log.warning(ex.getMessage());
         }
