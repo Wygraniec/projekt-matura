980c31ec00efdf530d7b718339cd57ccdd495187
Wygraniec <wojciech.legocki@gmail.com>
2024-09-12 18:26:00 +0200

    Added temporary proxy for insecure communication with backend

--- frontend/matura/vite.config.js
+++ frontend/matura/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/v1': {
+                target: 'http://business-rangers.gl.at.ply.gg:6383/api',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 })
